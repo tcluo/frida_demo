3881dd614c01fea385182fcde122a15fd376ff54
羅子竣 <tcluo@tcluo-macbook-pro.local>
2016-09-12 03:48:38 +0800

    remove demo cases

--- demo/basic/1-hook/agent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let addr
-
-Interceptor.attach(ptr(addr), {
-  onEnter(args) {
-    send(args[0].toInt32());
-  }
-});
--- demo/inject_object/structmod.py
+++ demo/inject_object/structmod.py
@@ -4,13 +4,9 @@ import sys
 
 session = frida.attach("client")
 script = session.create_script("""
-// First, let's give ourselves a bit of memory to put our struct in:
 send("Allocating memory and writing bytes...");
 var st = Memory.alloc(16);
-// Now we need to fill it - this is a bit blunt, but works...
 Memory.writeByteArray(st,[0x02, 0x00, 0x13, 0x89, 0x7F, 0x00, 0x00, 0x01, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30]);
-// Module.findExportByName() can find functions without knowing the source
-// module, but it's slower, especially over large binaries! YMMV...
 Interceptor.attach(Module.findExportByName(null, "connect"), {
     onEnter: function(args) {
         send("Injecting malicious byte array:");
