d6cf6446488d8881046f93cd7fea0414b724223c
羅子竣 <tcluo@tcluo-macbook-pro.local>
2016-09-12 02:07:37 +0800

    add anti-jb-detection

--- hook_demo/example.py
+++
@@ -1,7 +0,0 @@
-import frida
-session = frida.attach("cat")
-#print([x.name for x in session.enumerate_modules()])
-#print(session.enumerate_modules())
-#print(session.enumerate_ranges('rw-'))
-print session.read_bytes(4468781056, 10).encode("hex")
-#print session.write_bytes(4468781056, "frida")
